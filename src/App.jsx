6d57dbd6c86ba086694f5669bd25df081cc431a2
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-05-25 14:35:36 -0300

    Created pages directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { NavContainer } from "./style/Navbar"
 
-import HomePage from "./components/HomePage/HomePage"
-import SeatsPage from "./components/SeatsPage/SeatsPage"
-import SessionsPage from "./components/SessionsPage/SessionsPage"
-import SuccessPage from "./components/SuccessPage/SuccessPage"
+import HomePage from "./pages/HomePage/HomePage"
+import SeatsPage from "./pages/SeatsPage/SeatsPage"
+import SessionsPage from "./pages/SessionsPage/SessionsPage"
+import SuccessPage from "./pages/SuccessPage/SuccessPage"
 
 import axios from 'axios'
 
